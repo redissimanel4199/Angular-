c8e0559a0711fc9ddae5ec51c79cd7dccc0c0759
redissimanel4199 <47795369+redissimanel4199@users.noreply.github.com>
2022-11-10 17:52:19 +0100

    Update Stock.service.ts

--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'http://172.10.0.140:8083/SpringMVC/stock';
+  readonly API_URL = 'http://192.168.2.0:8083/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
